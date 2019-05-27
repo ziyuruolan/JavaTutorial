2a462abf370ed024364773f3549838bc52f5d79b
weizhaoquan <weizhaoquan@jd.com>
2019-05-27 20:31:30 +0800

    两数之和

--- src/main/java/cn/byhieg/arithmetic/TwoSum.java
+++ src/main/java/cn/byhieg/arithmetic/TwoSum.java
@@ -35,7 +35,7 @@ public class TwoSum {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int taget = 9;
-        int[] ints = twoSum1( nums, taget );
+        int[] ints = twoSum2( nums, taget );
         System.out.printf( Arrays.toString(ints) );
     }
 }
