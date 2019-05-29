8c93d26e5cd7963ec7bd11c6b6270dd9ac78e860
ziyuruolan <git3486686>
2019-05-29 08:16:19 +0800

    选择排序

--- src/main/java/cn/byhieg/arithmetic/sort/SelectSort.java
+++ src/main/java/cn/byhieg/arithmetic/sort/SelectSort.java
@@ -1,5 +1,7 @@
 package cn.byhieg.arithmetic.sort;
 
+import java.util.Arrays;
+
 public class SelectSort {
     static void swap(int[] arr,int i,int j){
         int temp = arr[i];
@@ -14,6 +16,40 @@ public class SelectSort {
                 minIndex = arr[j]<arr[minIndex] ? j : minIndex;
                 swap(arr,i,minIndex);
             }
+        }
+    }
+
+    static int[] selectSort1(int[] sourceArr){
+        int[] arr = Arrays.copyOf(sourceArr,sourceArr.length);
+        for (int i = 0;i<arr.length-1;i++) {
+            int min = i;
+            for(int j=i+1;j<arr.length;j++){
+                if (arr[j]<arr[min]) {
+                    min = j;
+                }
+            }
+            if (i!=min) {
+                int temp = arr[i];
+                arr[i]=arr[min];
+                arr[min]=temp;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6,3,5,7,3,7,8,9,1};
+        selectSort( arr );
+        System.out.printf( "1排序后的数组为 ：" );
+        for (int num :arr) {
+            System.out.printf( num + " " );
+        }
+        System.out.println(  );
+        int[] arr1 = {6,3,5,7,3,7,8,9,1};
+        int[] arr11 = selectSort1(arr1);
+        System.out.printf( "2排序后的数组为 ：" );
+        for (int num :arr11) {
+            System.out.printf( num+" " );
         }
     }
 }
